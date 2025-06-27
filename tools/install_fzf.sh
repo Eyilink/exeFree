41b083579a4183684e82feaeac69ef74d55489b0
Eyilink Eyilink <eyilink@outlook.com>
2025-06-28 00:19:10 +0200

    Fixing fzf-wordlist aalias

--- tools/install_fzf.sh
+++ tools/install_fzf.sh
@@ -8,7 +8,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 echo '# Search interactively in wordlists using fzf
 fzf-wordlist() {
   local wordlist
-  wordlist=$(find /opt/SecLists -type f ( -name "*.txt" -o -name "*.lst" ) | fzf --prompt="Choose a wordlist: ")
+  wordlist=$(find /opt/SecLists -type f -name "*.txt" | fzf --prompt="Choose a wordlist: ")
   [ -n "$wordlist" ] && cat "$wordlist" | fzf --prompt="Search word: "
 }
 ' >> ~/.zshrc
