5e1e788a659bdd4048ec3e97b8976e38636deef6
Eyilink Eyilink <eyilink@outlook.com>
2025-06-29 15:06:11 +0200

    Just getting the minimum wordlists

--- tools/install_fzf.sh
+++ tools/install_fzf.sh
@@ -2,6 +2,30 @@
 
 # sudo git clone https://github.com/danielmiessler/SecLists /opt/SecLists
 
+sudo mkdir -p /opt/SecLists
+cd /opt/SecLists
+
+# Subdomains
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/subdomains-top5000.txt
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/subdomains-top20000.txt
+
+# Raft directories
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/raft-small-directories.txt
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/raft-medium-directories.txt
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/raft-large-directories.txt
+
+# Directory listings
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/directory-listing-small.txt
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/directory-listing-medium.txt
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/directory-listing-large.txt
+
+# Web extensions
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/web-extensions.txt
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/web-extensions-big.txt
+
+# Burp parameters
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/refs/heads/master/Discovery/Web-Content/burp-parameter-names.txt
+
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
 
