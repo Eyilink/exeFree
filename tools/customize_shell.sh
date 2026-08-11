758839d4cd22ab4b78447ce22ca2791e9aa42071
Eyilink <38373463+Eyilink@users.noreply.github.com>
2026-08-11 13:17:49 +0200

    Update customize_shell.sh

--- tools/customize_shell.sh
+++ tools/customize_shell.sh
@@ -148,5 +148,5 @@ SAVEHIST=20000
 setopt INC_APPEND_HISTORY 
 setopt HIST_IGNORE_DUPS' >> ~/.zshrc
 
-bindkey "^[[1;5D" backward-word
-bindkey "^[[1;5C" forward-word
+echo 'bindkey "^[[1;5D" backward-word
+bindkey "^[[1;5C" forward-word' >> ~/.zshrc
