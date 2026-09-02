22638f917ed3b284a132d9c678564f7b51279677
Eyilink <38373463+Eyilink@users.noreply.github.com>
2026-09-03 00:38:44 +0200

    Update customize_shell.sh

--- tools/customize_shell.sh
+++ tools/customize_shell.sh
@@ -155,3 +155,5 @@ echo 'bindkey "^[[1;5C" forward-word' >> ~/.zshrc
 
 echo 'set -g status off' >> ~/.tmux.conf
 echo 'set -g xterm-keys on' >> ~/.tmux.conf
+echo 'set -g default-terminal "tmux-256color"' >> ~/.tmux.conf
+echo 'set -as terminal-overrides ',*:Tc'' >> ~/.tmux.conf
